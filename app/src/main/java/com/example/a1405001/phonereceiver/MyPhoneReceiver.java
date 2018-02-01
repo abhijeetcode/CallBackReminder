7f16c571390576733db403c4e13b8b0c9a4fb34c
abhijeetadhikary99@gmail.com <github@#bicky>
2018-02-01 20:48:36 +0530

    Dailog Added

--- app/src/main/java/com/example/a1405001/phonereceiver/MyPhoneReceiver.java
+++ app/src/main/java/com/example/a1405001/phonereceiver/MyPhoneReceiver.java
@@ -17,23 +17,21 @@ public class MyPhoneReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-
         Bundle extras = intent.getExtras();
-        if (extras != null && extras.getString(TelephonyManager.EXTRA_INCOMING_NUMBER)!= null) {
+        if (extras != null && extras.getString(TelephonyManager.EXTRA_INCOMING_NUMBER) != null) {
             String state = extras.getString(TelephonyManager.EXTRA_STATE);
             phoneNumber = extras.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
             if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
                 Log.v("Phone Ringing", phoneNumber);
-            }
-            else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
-                Log.v("Phone Missed", phoneNumber+"");
+            } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                Log.v("Phone Missed", phoneNumber + "");
                 displayDialog(context);
             }
         }
     }
 
-    private void displayDialog(Context context){
-        Intent i = new Intent(context,Promtbox.class);
+    private void displayDialog(Context context) {
+        Intent i = new Intent(context, Promtbox.class);
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(i);
     }
--- app/src/main/java/com/example/a1405001/phonereceiver/Promtbox.java
+++ app/src/main/java/com/example/a1405001/phonereceiver/Promtbox.java
@@ -9,30 +9,55 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import static android.provider.ContactsContract.Intents.Insert.ACTION;
 
 public class Promtbox extends Activity {
+    AlertDialog alertDialog;
+    private String selectedItem;
+    final CharSequence[] items = {" 15 min later ", " 30 min later ", " Coustomized "};
+    MyPhoneReceiver myPhoneReceiver = new MyPhoneReceiver();
+    String number = myPhoneReceiver.phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AlertDialog levelDialog;
+        // Creating and Building the Dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder
-                .setTitle("Test")
-                .setMessage("Are you sure you want to exit?")
-                .setCancelable(false)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
+        builder.setTitle("Select The Time");
+        builder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int num) {
+                switch (num) {
+                    case 0:
+                        selectedItem = (String) items[num];
+                        break;
+                    case 1:
+                        selectedItem = (String) items[num];
+                        break;
+                    case 2:
+                        selectedItem = (String) items[num];
+                        break;
+                }
+            }
+        });
+        builder.
+                setPositiveButton("Set", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), " " + selectedItem, Toast.LENGTH_LONG).show();
+                        dialogInterface.cancel();
                     }
                 })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
                     }
                 });
-        AlertDialog alert = builder.create();
-        alert.show();
+        levelDialog = builder.create();
+        levelDialog.show();
     }
 }
