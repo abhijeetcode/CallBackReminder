ca134e4669f37e172d8073aa45485e06e64c8613
abhijeetadhikary99@gmail.com <github@#bicky>
2018-02-02 21:09:54 +0530

    Dailog Added

--- app/src/main/java/com/example/a1405001/phonereceiver/MyPhoneReceiver.java
+++ app/src/main/java/com/example/a1405001/phonereceiver/MyPhoneReceiver.java
@@ -10,28 +10,31 @@ import android.util.Log;
 /**
  * Created by 1405001 on 01-02-2018.
  */
-
 public class MyPhoneReceiver extends BroadcastReceiver {
     String phoneNumber;
+    Promtbox promtbox = new Promtbox();
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
         Bundle extras = intent.getExtras();
         if (extras != null && extras.getString(TelephonyManager.EXTRA_INCOMING_NUMBER) != null) {
             String state = extras.getString(TelephonyManager.EXTRA_STATE);
             phoneNumber = extras.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+
+            Log.d("Mobile Number"," "+promtbox.phone_number);
             if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                phoneNumber = extras.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
                 Log.v("Phone Ringing", phoneNumber);
             } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                phoneNumber = extras.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
                 Log.v("Phone Missed", phoneNumber + "");
                 displayDialog(context);
             }
         }
     }
-
     private void displayDialog(Context context) {
         Intent i = new Intent(context, Promtbox.class);
+        i.putExtra("phoneNumber",phoneNumber);
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(i);
     }
--- app/src/main/java/com/example/a1405001/phonereceiver/Promtbox.java
+++ app/src/main/java/com/example/a1405001/phonereceiver/Promtbox.java
@@ -2,36 +2,74 @@ package com.example.a1405001.phonereceiver;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
-import static android.provider.ContactsContract.Intents.Insert.ACTION;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 
-public class Promtbox extends Activity implements TimePickerDialog.OnTimeSetListener,DatePickerDialog.OnDateSetListener {
+import shortroid.com.shortroid.ShortRoidDB.ShortRoidDB;
+
+public class Promtbox extends Activity {
     AlertDialog alertDialog;
     private String selectedItem;
-    final CharSequence[] items = {" 15 min later ", " 30 min later ", " Coustomized "};
-    MyPhoneReceiver myPhoneReceiver = new MyPhoneReceiver();
-    String number = myPhoneReceiver.phoneNumber;
+    Intent intent;
+    final CharSequence[] items = {"15 min later", "30 min later", "Coustomized"};
+    //MyPhoneReceiver myPhoneReceiver = new MyPhoneReceiver();
+    String phone_number;
+    private Context context;
+    private static final String TIME_PATTERN = "HH:mm";
+    private Calendar calendar;
+    private DateFormat dateFormat;
+    private SimpleDateFormat timeFormat;
+    int hours;
+    int mins;
+    int month;
+    int year;
+    int day;
+    Date dateTime;
+    HashMap<String, String> attributes;
+    HashMap<String, Object> data;
+    ShortRoidDB shortdb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AlertDialog levelDialog;
         // Creating and Building the Dialog
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Select The Time");
+        //Database
+        intent = getIntent();
+        attributes = new HashMap<>();
+        phone_number = intent.getStringExtra("phoneNumber");
+        attributes.put("NUMBER", "TEXT NOT NULL");
+        attributes.put("MINUTE", "INT");
+        attributes.put("HOUR", "INT");
+        attributes.put("DATE", "INT");
+        attributes.put("MONTH", "INT");
+        attributes.put("YEAR", "INT");
+        shortdb = new ShortRoidDB(Promtbox.this, "CallbackDetails", 1, "Miscall", attributes);
+
+        calendar = Calendar.getInstance();
+        dateFormat = DateFormat.getDateInstance(DateFormat.LONG, Locale.getDefault());
+        timeFormat = new SimpleDateFormat(TIME_PATTERN, Locale.getDefault());
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(phone_number + " Callback After");
         builder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int num) {
+                AlertDialog alertDialog = new AlertDialog.Builder(Promtbox.this).create(); //Read Update
                 switch (num) {
                     case 0:
                         selectedItem = (String) items[num];
@@ -49,7 +87,41 @@ public class Promtbox extends Activity implements TimePickerDialog.OnTimeSetList
                 setPositiveButton("Set", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(getApplicationContext(), " " + selectedItem, Toast.LENGTH_LONG).show();
+                        String s;
+                        Calendar now = Calendar.getInstance();
+                        SimpleDateFormat df = new SimpleDateFormat("hh:mm:ss");
+                        if (selectedItem == "Coustomized") {
+                            DisplayTimeDate();
+                        } else if (selectedItem == "15 min later") {
+                            year = now.get(Calendar.YEAR);
+                            month = (now.get(Calendar.MONTH) + 1);
+                            day = now.get(Calendar.DATE);
+                            now.add(Calendar.MINUTE, 15);
+                            s = df.format(now.getTime());
+                            String[] arrOfStr = s.split(":");
+                            mins = Integer.parseInt((arrOfStr[0]));
+                            hours = Integer.parseInt((arrOfStr[1]));
+                            Log.d("Nowtime ", " " + phone_number);
+                            //Insert
+                            //InsertData(phone_number, mins, hours, year, month, day);
+                            //Toast.makeText(getApplicationContext(), " " + mins + " " + hours, Toast.LENGTH_LONG).show();
+                        } else {
+                            now.add(Calendar.MINUTE, 30);
+                            year = now.get(Calendar.YEAR);
+                            month = (now.get(Calendar.MONTH) + 1);
+                            day = now.get(Calendar.DATE);
+                            s = df.format(now.getTime());
+                            String[] arrOfStr = s.split(":");
+                            //Log.d("Nowtime ",arrOfStr[0]+" "+arrOfStr[1]);
+                            mins = Integer.parseInt((arrOfStr[0]));
+                            hours = Integer.parseInt((arrOfStr[1]));
+                            Log.d("Nowtime ", " " + phone_number);
+                            //Insert
+                            //InsertData(phone_number, mins, hours, year, month, day);
+                            Log.d("Now Time", mins + " " + hours);
+                            //Log.d("PhoneNumber",phone_number);
+                            //Toast.makeText(getApplicationContext(), " " + s + " " + day + " " + month + " " + year, Toast.LENGTH_LONG).show();
+                        }
                         dialogInterface.cancel();
                     }
                 })
@@ -63,13 +135,44 @@ public class Promtbox extends Activity implements TimePickerDialog.OnTimeSetList
         levelDialog.show();
     }
 
-    @Override
-    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+    public void DisplayTimeDate() {
+
+        final View dialogView = View.inflate(this, R.layout.time_date_picker, null);
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        dialogView.findViewById(R.id.date_time_set).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePicker datePicker = (DatePicker) dialogView.findViewById(R.id.date_picker);
+                TimePicker timePicker = (TimePicker) dialogView.findViewById(R.id.time_picker);
+
+                Calendar calendar = new GregorianCalendar(datePicker.getYear(),
+                        datePicker.getMonth(),
+                        datePicker.getDayOfMonth(),
+                        timePicker.getCurrentHour(),
+                        timePicker.getCurrentMinute());
+                Date dateTime = calendar.getTime();
+                year = calendar.get(Calendar.YEAR);
+                month = (calendar.get(Calendar.MONTH) + 1);
+                day = calendar.get(Calendar.DATE);
+                mins = dateTime.getMinutes();
+                hours = dateTime.getHours();
+                Log.v("DateTimePicker Debugg", "" + dateTime);
+                Log.v("DateTimePicker Debugg", "" + day + " " + month + " " + year + " " + hours + " " + mins);
+                //InsertData(phone_number, year, month, day, hours, mins);
+                alertDialog.dismiss();
+            }
+        });
+        alertDialog.setView(dialogView);
+        alertDialog.show();
 
     }
 
-    @Override
-    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-
+    public void InsertData(String phone_number, int mins, int hours, int year, int month, int day) {
+        attributes.put("NUMBER", phone_number);
+        data.put("MINUTE", mins);
+        data.put("HOUR", hours);
+        data.put("DATE", year);
+        data.put("MONTH", month);
+        data.put("YEAR", day);
     }
 }
