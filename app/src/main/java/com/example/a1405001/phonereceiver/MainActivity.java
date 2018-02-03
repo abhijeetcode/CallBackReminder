bbd0700a161b617f1f662b62a5fa63ab70b2780a
abhijeetadhikary99@gmail.com <github@#bicky>
2018-02-03 14:00:16 +0530

    Reminder Delete option Added

--- app/src/main/java/com/example/a1405001/phonereceiver/MainActivity.java
+++ app/src/main/java/com/example/a1405001/phonereceiver/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.a1405001.phonereceiver;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -49,16 +51,56 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        mAdapter.notifyDataSetChanged();
         recyclerView.setAdapter(mAdapter);
         recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+            //RecyclerView OnClick Event
             @Override
             public void onClick(View view, int position) {
                 CallBackDetails movie = callList.get(position);
-                //After click go to Another Activity
+                final String t_time, d_date, m_no;
+                m_no = movie.getMoblieNumber();
+                t_time = movie.getTime();
+                d_date = movie.getDate();
+                //DialogBox
                 Toast.makeText(getApplicationContext(), movie.getMoblieNumber() + " is selected!", Toast.LENGTH_SHORT).show();
-                Intent myIntent = new Intent(MainActivity.this, ReminderUpdateRemove.class);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Do you want to delete")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //Delete from database
+                                String query = "DELETE FROM Miscall WHERE NUMBER = '"+m_no+"';";
+                                //String query = "update Miscall set NUMBER = 123 where NUMBER = "+m_no+";";
+                                boolean b = shortdb.anyQuery(query);
+                                Log.d("querydelete ",b+" "+query);
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //  Action for 'NO' Button
+                                finish();
+                                //dialog.cancel();
+                            }
+                        });
+
+                //Creating dialog box
+                AlertDialog alert = builder.create();
+                //Setting the title manually
+                alert.setTitle(m_no);
+                alert.show();
+                setContentView(R.layout.activity_main);
+
+                /*Intent myIntent = new Intent(MainActivity.this, ReminderUpdateRemove.class);
+                myIntent.putExtra("phoneNumber", m_no);
+                myIntent.putExtra("time", t_time);
+                myIntent.putExtra("date", d_date);
+                //After click go to Another Activity
                 startActivity(myIntent);
+                */
+
             }
+
             @Override
             public void onLongClick(View view, int position) {
             }
@@ -82,22 +124,4 @@ public class MainActivity extends AppCompatActivity {
             callList.add(callBackDetails);
         }
     }
-
-    public void sendData()
-    {
-        CallBackDetails callBackDetails;
-        callBackDetails = new CallBackDetails("+919692209926","8:30", "11/08/1994");
-        callList.add(callBackDetails);
-
-        callBackDetails = new CallBackDetails("9692209926","8:30", "11/08/1994");
-        callList.add(callBackDetails);
-
-        callBackDetails = new CallBackDetails("9692209926","8:30", "11/08/1994");
-        callList.add(callBackDetails);
-
-        callBackDetails = new CallBackDetails("9692209926","8:30", "11/08/1994");
-        callList.add(callBackDetails);
-    }
-
-
 }
--- app/src/main/java/com/example/a1405001/phonereceiver/ReminderUpdateRemove.java
+++ app/src/main/java/com/example/a1405001/phonereceiver/ReminderUpdateRemove.java
@@ -1,13 +1,23 @@
 package com.example.a1405001.phonereceiver;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReminderUpdateRemove extends AppCompatActivity {
+    Intent intent;
+    TextView textView1,textView2,textView3;
+    String moblieNumber,time,date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder_update_remove);
+        intent = getIntent();
+        moblieNumber = intent.getStringExtra("phoneNumber");
+        time = intent.getStringExtra("time");
+        date = intent.getStringExtra("date");
+
     }
 }
